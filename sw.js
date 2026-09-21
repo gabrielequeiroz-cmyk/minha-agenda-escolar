7429ac0927e30cbf51db9fea376615c5ea92bbd7
gabi346 <bih45qlima@gmail.com>
2026-09-21 17:35:11 -0400

    create cache

--- sw.js
+++ sw.js
@@ -1,5 +1,21 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker instalado");
+const CACHE_NAME = "agenda-v1";
+
+const ARQUIVOS = [
+    "index.html",
+    "style.css",
+    "app.js"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => {
+            return cache.addAll(
+                ARQUIVOS
+
+            );
+        })
+    );
 });
 
 self.addEventListener("active", (event) => {
@@ -11,8 +27,10 @@ self.addEventListener("fetch", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-    if (event.request.url.endsWith("/teste-sw")) {
-        event.respondWith(new Response("Resposta criada pelo Service Worker!"));
-        return;
-    }
+    event.respondWith(
+        caches.match(event.request)
+        .then((resposta) => {
+            return resposta || fetch(event.request);
+        })
+    );
 });
